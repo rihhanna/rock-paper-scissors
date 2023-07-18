4a47e1d1677ad5e4ff9303f4dac15d9a31377ed4
rehhana <ashiqtalmustahiil@gmail.com>
2023-07-17 20:35:58 -0700

    answers of rock-paper-scissoes

--- challenge.js
+++ challenge.js
@@ -1,2 +1,14 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+function playGeme(player1,player2){
+    if (player1==='Rock' && player2==='paper'){
+        return 'player2 won'
+    }else if(player1==="scissors"&&player2==="Rock"){
+        return 'player1 won'
+    }else{
+        return 'return tie'
+    }
+}
+ console.log(playGeme('Rock','paper'))
+console.log(playGeme('scissors','scissors'))
